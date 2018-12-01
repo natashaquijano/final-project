24069ba7f81fc1b551dbb1ea28f660fbe940f94a
Natasha Quijano <niquijanodesign@gmail.com>
2018-12-01 15:31:29 -0800

    fixed header and video of landing and home page as well asfollowing and wishlist

--- client/src/components/HeaderLandingVideo/HeaderLandingVideo.js
+++ client/src/components/HeaderLandingVideo/HeaderLandingVideo.js
@@ -5,10 +5,12 @@ import '../HeaderLandingVideo/HeaderLandingVideo.scss';
 class HeaderLandingVideo extends Component {
     render() {
         return (
-            <div className="vid2">
+            <div className="wrapper2">
                 <video className="mainvideo2" loop autoPlay="main-video2" src={Video} alt="video" />
-                <input type="text" placeholder="Your Location" name="search-location" id="location-bar2" />
-                <button className="save-button2">Save</button>
+                <div className="button-save">
+                    <input type="text" placeholder="Your Location" name="search-location" id="location-bar2" />
+                    <button className="save-button2">Save</button>
+                </div>
             </div>
         );
     }
