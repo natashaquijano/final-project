03e6ad5ebc319a31a54c70a396dd557dd0949784
Natasha Quijano <niquijanodesign@gmail.com>
2018-12-01 19:46:40 -0800

    updated profile wishlist

--- client/src/components/WishlistProfile/WishlistProfile.js
+++ client/src/components/WishlistProfile/WishlistProfile.js
@@ -11,7 +11,7 @@ class WishlistProfile extends Component {
             <div>
                 <div className="wish-title-view">
                     <div className="wishlistp-title">
-                        <h1 className="wishlistp-title">Wishlist</h1>
+                        <h1 className="wishlistp-title">Likes</h1>
                     </div>
                     <div className="wishlist-viewall">
                         <p className="wishlist-viewall">View All</p>
@@ -39,7 +39,7 @@ class WishlistProfile extends Component {
                 </div>
                 <div className="wish-title-view">
                     <div className="wishlistp-title">
-                        <h1 className="wishlistp-title">Wishlist</h1>
+                        <h1 className="wishlistp-title">Upcoming Events</h1>
                     </div>
                     <div className="wishlist-viewall">
                         <p className="wishlist-viewall">View All</p>
@@ -53,32 +53,19 @@ class WishlistProfile extends Component {
                 </div>
                 <div className="wish-title-view">
                     <div className="wishlistp-title">
-                        <h1 className="wishlistp-title">Wishlist</h1>
+                        <h1 className="wishlistp-title">Order History</h1>
                     </div>
                     <div className="wishlist-viewall">
                         <p className="wishlist-viewall">View All</p>
                     </div>
                 </div>
-                <div className="wishlist-profile-images">
+                <div className="wishlist-profile-images bottom-images">
                     <img className="wishlist-profile-images-1" src={Rumors} alt="followers" /><p className="followers followers-p"></p>
                     <img className="wishlist-profile-images-1" src={PopUp} alt="followers" /><p className="followers followers-p"></p>
                     <img className="wishlist-profile-images-1" src={Yoga} alt="followers" /><p className="followers followers-p"></p>
                     {/* <img className="wishlist-profile-images-1" src={Followers} alt="followers" /><p className="followers followers-p"></p> */}
                 </div>
-                <div className="wish-title-view">
-                    <div className="wishlistp-title">
-                        <h1 className="wishlistp-title">Wishlist</h1>
-                    </div>
-                    <div className="wishlist-viewall">
-                        <p className="wishlist-viewall">View All</p>
-                    </div>
-                </div>
-                <div className="wishlist-profile-images">
-                    <img className="wishlist-profile-images-1" src={Rumors} alt="followers" /><p className="followers followers-p"></p>
-                    <img className="wishlist-profile-images-1" src={PopUp} alt="followers" /><p className="followers followers-p"></p>
-                    <img className="wishlist-profile-images-1" src={Yoga} alt="followers" /><p className="followers followers-p"></p>
-                    {/* <img className="wishlist-profile-images-1" src={Followers} alt="followers" /><p className="followers followers-p"></p> */}
-                </div>
+
             </div>
         );
     }
