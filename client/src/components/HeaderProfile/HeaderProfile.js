30bf8b0c873c03790b7218962372c76676d3c0cf
Natasha Quijano <niquijanodesign@gmail.com>
2018-12-05 14:32:20 -0800

    fixed cover/profile info

--- client/src/components/HeaderProfile/HeaderProfile.js
+++ client/src/components/HeaderProfile/HeaderProfile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './HeaderProfile.scss';
 
 import defaultCover from '../../images/ProfileActivity/default-cover.png'
+import Defi from '../../images/icons/default-image.png';
 
 // import Following from '../../components/Following/Following';
 
@@ -14,6 +15,12 @@ class HeaderProfile extends Component {
                     <div className="pro-img pro-img1">
                         {/* <video className="mainprofile" loop autoPlay="main-video" src={Video} alt="video" /> */}
                         <img src={defaultCover} className="pro-img1" alt="articles" />
+                    </div>
+                    <div>
+                        <h1 className="default-user-info">Username</h1>
+                        <h2 className="default-user-info info2">Full Name</h2>
+                        <h2 className="default-user-info info3">Location</h2>
+                        <img className="nav-image nav-image2" src={Defi} alt="defi" />
                     </div>
                 </div>
                 <div>
