813a8e548860d2a4f970db952afcd2f97c11576f
Natasha Quijano <niquijanodesign@gmail.com>
2018-12-14 19:19:50 -0800

    fixed footer styling

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -12,27 +12,27 @@ class Footer extends Component {
         return (
             <div className="Footer">
                 {/* <div class="footer-logo"> */}
+                <div>
+                    <img className="logo-footer" src={FooterLogo} alt="Instagram" />
+                    {/* </div> */}
 
-                <img className="logo-footer" src={FooterLogo} alt="Instagram" />
-                {/* </div> */}
-
-                {/* <div className="footer-description"> */}
-                <h2 className="footer-title">Rouse</h2>
-                {/* </div> */}
-
-                {/* <div className="footer-social">
+                    {/* <div className="footer-description"> */}
+                    <h2 className="footer-title">Rouse</h2>
+                    {/* </div> */}
+                </div>
+                {/* <div cla</div>ssName="footer-social">
                     <div className="social-title"> */}
                 {/* <h1 className="social">Follow Us</h1> */}
                 {/* </div> */}
                 {/* <div className="social-icons"> */}
-                <div className="div2">
-                    <ul className="social-ul">
-                        <li className="social-icons-footer instagram"><img className="instagram social-icons-footer" src={Instagram} alt="Instagram" /></li>
-                        <li className="social-icons-footer facebook"><img className="facebook social-icons-footer" src={Facebook} alt="Facebook" /></li>
-                        <li className="twitter social-icons-footer twitter"><img className="twitter social-icons-footer" src={Twitter} alt="Twitter" /></li>
-                    </ul>
-                    {/* </div> */}
-                </div>
+
+                <ul className="social-ul">
+                    <li className="social-icons-footer instagram"><img className="instagram social-icons-footer" src={Instagram} alt="Instagram" /></li>
+                    <li className="social-icons-footer facebook"><img className="facebook social-icons-footer" src={Facebook} alt="Facebook" /></li>
+                    <li className="twitter social-icons-footer twitter"><img className="twitter social-icons-footer" src={Twitter} alt="Twitter" /></li>
+                </ul>
+                {/* </div> */}
+
             </div >
 
 
