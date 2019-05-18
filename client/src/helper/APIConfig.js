0bb7c556e739babb8038c1bc96b307486165630e
Natasha Quijano <niquijanodesign@gmail.com>
2019-05-17 20:49:28 -0700

    backend route

--- client/src/helper/APIConfig.js
+++ client/src/helper/APIConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: process.env.api || "http://localhost:4000/api"
+    baseURL: process.env.api || "https://rouse-backend-website.herokuapp.com/api"
 })
 export default axiosInstance;
